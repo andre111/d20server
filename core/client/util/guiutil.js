d3be1117b723a8b43d5629c9eae9925c37a7a84e
Andre Schweiger <andre.spiele@yahoo.de>
2021-02-13 17:20:23 +0100

    Change tooltips to fixed position and apply some styling

--- core/client/util/guiutil.js
+++ core/client/util/guiutil.js
@@ -83,19 +83,37 @@ export const GuiUtils = {
     
     // makes children elements hoverable (requires 'hoverable' class and a child with 'onhover' class)
     makeHoverable: function(element) {
+        // this updates to "tooltip" location
+        const updatePosition = (event, hoverable, onhover) => {
+            const width = onhover.clientWidth;
+            const height = onhover.clientHeight;
+
+            // mouse based position
+            //var x = event.clientX - width / 2;
+            //var y = event.clientY;
+            // fixed position
+            const br = hoverable.getBoundingClientRect();
+            var x = br.left + (br.right - br.left) / 2 - width / 2;
+            var y = br.top - height - 5;
+
+            if(x < 0) x = 0;
+            if(y < 0) y = 0;
+            if(x+width > window.innerWidth) x = window.innerWidth - width;
+            if(y+height > window.innerHeight) y = window.innerHeight - height;
+
+            onhover.style.left = x+'px';
+            onhover.style.top = y+'px';
+        };
+
+        // find all hoverable elements
         for(const e of element.getElementsByClassName('hoverable')) {
             e.onmouseover = (event) => {
                 const onhover = e.getElementsByClassName('onhover')[0];
-                const width = onhover.clientWidth;
-                const height = onhover.clientHeight;
-                var x = event.clientX - width / 2;
-                var y = event.clientY;
-                
-                if(x+width > window.innerWidth) x = window.innerWidth - width;
-                if(y+height > window.innerHeight) y = window.innerHeight - height;
-
-                onhover.style.left = x+'px';
-                onhover.style.top = y+'px';
+                updatePosition(event, e, onhover); //TODO: this does not always calculate the correct location (on first go, hovering a second time seems to fix that)
+                //TODO: remove, this tries to fix this position after 
+                //      while this "fixes" the issue mentioned above, it causes short jumping/glitching graphics and looks bad
+                //      but currently this is masked by the tooltip fade
+                setTimeout(() => updatePosition(event, e, onhover), 1);
             };
         }
     }
