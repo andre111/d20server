14112821c90742ed9321ffe2e0adade4c850db1f
Andre Schweiger <andre.spiele@yahoo.de>
2021-05-01 15:35:17 +0200

    Fix/Avoid crashing on race condition where contained actor has not been synched to client yet (->entitymanager does not exist yet)

--- core/common/util/tokenutil.js
+++ core/common/util/tokenutil.js
@@ -7,7 +7,10 @@ export class TokenUtil {
         if(!token || !(token instanceof Entity) || token.getType() !== 'token') throw new Error('Provided object is not a token');
 
         if(token.getBoolean('actorLocal')) {
-            return EntityManagers.get('token/'+token.getID()+'-actor').find(1);
+            const manager = EntityManagers.get('token/'+token.getID()+'-actor');
+            if(!manager) return null; // avoid crashing on race conditions where the entitymanager has not been synched/created on client yet
+
+            return manager.find(1);
         } else {
             const actorID = token.getLong('actorID');
             return EntityManagers.get('actor').find(actorID);
